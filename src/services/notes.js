b6649cc8281c39281a5947d1f9f11ef961681c59
KKato <kazuya.kato@serverworks.co.jp>
2019-05-01 13:03:50 +0900

    define empty function in notes.js

---
+++ src/services/notes.js
@@ -0,0 +1,15 @@
+const save = () => {
+  return null
+}
+const get= () => {
+  return null
+}
+const list= () => {
+  return null
+}
+
+export const notes = {
+  save: save,
+  get: get,
+  list: list
+}
