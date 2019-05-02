d44dfbf24c5d75d5a6e8a5a1d18e2876302d2e03
KKato <kazuya.kato@serverworks.co.jp>
2019-05-03 02:32:04 +0900

    update save test

--- tests/notes.test.js
+++ tests/notes.test.js
@@ -4,18 +4,50 @@ import dummy from './dummy.js'
 describe('note', () => {
   // notes.save()
   describe('save()', () => {
+    // create
+    describe('isn\'t passed id', () => {
+      it('returns success status when success', () => {
+        const data = dummy.createDummy
+        expect.assertions(3)
+        return note.save(data).then(result => {
+          // args don't have id
+          expect(data.id).toBeFalsy()
+          expect(data.title).toBeTruthy()
+          expect(result.status).toBe('success')
+        })
+      })
 
-    it('returns success status when success', () => {
-      expect.assertions(1)
-      return note.save(dummy.createDummy).then(result => {
-        expect(result.status).toBe('success')
+      it('returns an error when wrong arguments', () => {
+        const data = dummy.createErrDummy
+        expect.assertions(3)
+        return note.save(data).catch(err => {
+          // args don't have title
+          expect(data.id).toBeFalsy()
+          expect(data.title).toBeFalsy()
+          expect(err.message).toMatch('error')
+        })
       })
     })
+    // update
+    describe('passed id', () => {
+      it('returns success status when success', () => {
+        const data = dummy.updateDummy
+        expect.assertions(3)
+        return note.save(data).then(result => {
+          expect(data.id).toBeTruthy()
+          expect(data.title).toBeTruthy()
+          expect(result.status).toBe('success')
+        })
+      })
 
-    it('returns an error when wrong arguments', () => {
-      expect.assertions(1)
-      return note.save(dummy.createErrDummy).catch(err => {
-        expect(err.message).toMatch('error')
+      it('returns an error when wrong arguments', () => {
+        const data = dummy.updateErrDummy
+        expect.assertions(3)
+        return note.save(data).catch(err => {
+          expect(data.id).toBeTruthy()
+          expect(data.title).toBeFalsy()
+          expect(err.message).toMatch('error')
+        })
       })
     })
   })
