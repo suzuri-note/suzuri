9818fa60d5d587cafe6681ab4990a4cab17bbdf4
kecy <keishrio.oyama@linecorp.com>
2019-05-03 22:55:54 +0900

    :+1: CSSフレームワーク最適化

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,29 @@
 <template>
     <nav class="navbar bg-dark">
-      <a href="#" class="navbar-brand text-brand">SUZURI</a>
-      <i class="fas fa-search icon-brand"></i>
+      <div class="navbar-brand">
+        <a href="#" class="navbar-item text-brand">SUZURI</a>
+      </div>
+      <div class="navbar-menu">
+        <button type="button" class="icon-btn icon-btn-primary"><i class="fas fa-search"></i></button>
+        
+      </div>
     </nav>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .navbar {
   z-index: 1000;
+  position: relative;
+  display: -ms-flexbox;
+  display: flex;
+  flex-direction: row;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+  -ms-flex-align: center;
+  align-items: center;
+  -ms-flex-pack: justify;
+  justify-content: space-between;
+  padding: .5rem 1rem;
 }
 
 .navbar-brand {
