43501b6213fcc37fe942c4280ef6f493628788a5
kecy <keishrio.oyama@linecorp.com>
2019-06-15 22:51:19 +0900

    Resolve Warning in services

--- src/services/note.ts
+++ src/services/note.ts
@@ -1,27 +1,30 @@
 import shortid from 'shortid';
 
 interface INote {
-  title: string
-  body: string
-  id: string,
-  createdAt: number,
-  updatedAt: number
+  title: string;
+  body: string;
+  id: string;
+  createdAt: number;
+  updatedAt: number;
 }
+
 interface INoteID {
-  id: string
+  id: string;
 }
 
 interface ISingleResponse {
-  status: string
-  data: INote
+  status: string;
+  data: INote;
 }
+
 interface IListResponse {
-  status: string
-  data: INote[]
+  status: string;
+  data: INote[];
 }
+
 interface IIDResponse {
-  status: string
-  data: INoteID
+  status: string;
+  data: INoteID;
 }
 
 const prefix = 'SuzuriMemoId.';
@@ -30,112 +33,110 @@ const save = (data: any): Promise<ISingleResponse> => {
   return new  Promise((resolve: (value?: ISingleResponse) => void, reject: (reason?: any) => void) => {
     // check data construnction
     const required = ['title', 'body'];
-    const checkKeys = (data: any, keys: string[]) => {
-      const resultKeys = keys.filter((key) => { if (!data[key])  return key })
+    const checkKeys = (checkdata: any, keys: string[]) => {
+      const resultKeys = keys.filter((key) => { if (!checkdata[key]) {  return key; } });
       if (resultKeys.length) {
-        throw new Error('Don\'t have keys error: ' + resultKeys.join(', '))
+        throw new Error('Don\'t have keys error: ' + resultKeys.join(', '));
       }
     };
     // save data
-    const saveLocalStorage = (data: any) => {
+    const saveLocalStorage = (savedata: any) => {
       const now = Date.now();
-      data.updatedAt = now;
+      savedata.updatedAt = now;
       // create
-      if(!data.id) {
-        data.id = shortid.generate();
-        data.createdAt = now;
-      }
-      // update
-      else {
-        let originData = JSON.parse(localStorage.getItem(prefix + data.id)!);
-        data = Object.assign(originData, data);
+      if (!savedata.id) {
+        savedata.id = shortid.generate();
+        savedata.createdAt = now;
+      } else {
+        const originData = JSON.parse(localStorage.getItem(prefix + savedata.id)!);
+        savedata = Object.assign(originData, savedata);
       }
       // create data for localStorage
-      const key = data.id;
-      const value = JSON.stringify(data);
+      const key = savedata.id;
+      const value = JSON.stringify(savedata);
       // save
       localStorage.setItem(prefix + key, value);
       // return value
-      return data;
-    }
+      return savedata;
+    };
     // main
     try {
       checkKeys(data, required);
       const res = saveLocalStorage(data);
       resolve({
-        status: "success",
-        data: res
+        status: 'success',
+        data: res,
       });
-    } catch(err) {
+    } catch (err) {
       reject(err);
     }
   });
-}
+};
 const get = (id: string): Promise<ISingleResponse> => {
   return new Promise((resolve: (value?: ISingleResponse) => void, reject: (reason?: any) => void) => {
     // get Data
-    const getLocalStorage = (id: string) => {
-      let data = JSON.parse(localStorage.getItem(prefix + id)!);
-      if (!data) throw new Error("this id is not available.");
+    const getLocalStorage = (itemID: string) => {
+      const data = JSON.parse(localStorage.getItem(prefix + itemID)!);
+      if (!data) { throw new Error('this id is not available.'); }
       return data;
-    }
+    };
     try {
       const res = getLocalStorage(id);
       resolve({
-        status: "success",
-        data: res
+        status: 'success',
+        data: res,
       });
-    } catch(err) {
+    } catch (err) {
       reject(err);
     }
   });
-}
+};
 const list = (): Promise<IListResponse> => {
   return new Promise((resolve: (value?: IListResponse) => void, reject: (reason?: any) => void) => {
     // list Data
     const listLocalStorage = () => {
       const correctKeys = Object.keys(localStorage).filter((key) => {
         return key.startsWith(prefix);
-      })
+      });
       const memos = correctKeys.map((key) => {
-        let data = JSON.parse(localStorage.getItem(key)!);
+        const data = JSON.parse(localStorage.getItem(key)!);
         return data;
-      })
+      });
       return memos;
     };
     try {
       const res = listLocalStorage();
       resolve({
-        status: "success",
-        data: res
+        status: 'success',
+        data: res,
       });
-    } catch(err) {
+    } catch (err) {
       reject(err);
     }
-  })
-}
+  });
+};
 
 const remove = async (id: string): Promise<IIDResponse> => {
   return new Promise((resolve: (value?: IIDResponse) => void, reject: (reason?: any) => void) => {
     // remove Data
-    const removeLocalStorage = (id: string) => {
-      if (Object.keys(localStorage).indexOf(prefix + id) == -1) {
+    const removeLocalStorage = (itemID: string) => {
+      if (Object.keys(localStorage).indexOf(prefix + itemID) === -1) {
         throw new Error('this id is not available.');
       }
-      localStorage.removeItem(prefix + id);
-      return { id };
+      localStorage.removeItem(prefix + itemID);
+      return { id: itemID };
     };
     try {
       const res = removeLocalStorage(id);
       resolve({
-        status: "success",
-        data: res
+        status: 'success',
+        data: res,
       });
-    } catch(err) {
+    } catch (err) {
       reject(err);
     }
-  })
-}
+  });
+};
 
 export default {
   save,
--- tests/dummy.ts
+++ tests/dummy.ts
@@ -1,18 +1,18 @@
 import shortid from 'shortid';
 
 const deleteItem = (obj: any, keys: string[]) => {
-  const newObj = Object.assign({}, obj)
-  for (let key of keys) {
-    delete newObj[key]
+  const newObj = Object.assign({}, obj);
+  for (const key of keys) {
+    delete newObj[key];
   }
-  return newObj
-}
+  return newObj;
+};
 
 const dummyMemos = [
   // first data
   {
     id: '',
-    title: "数体である定義体",
+    title: '数体である定義体',
     body: `複素数体であれば、任意のCM-タイプの A は、実際、[数体](https://ja.wikipedia.org/wiki/%E4%BB%A3%E6%95%B0%E4%BD%93)である定義体（英語版）(field of definition)を持っている。自己準同型環の可能なタイプは、対合（ロサチの対合（英語版）(Rosati involution）をもつ環として既に分類されていて、CM-タイプのアーベル多様体の分類を導き出す。
 
   * 演算
@@ -26,7 +26,7 @@ const dummyMemos = [
   // update data
   {
     id: '',
-    title: "数体や大域函数体",
+    title: '数体や大域函数体',
     body: `数論では、ヘッケ指標(Hecke character)はディリクレ指標の一般化であり、エーリッヒ・ヘッケ（英語版）(Erich Hecke)によりディリクレのL-函数よりも大きな L-函数のクラスを構成するために導入された。
 
 * イデールを使う定義
@@ -36,8 +36,8 @@ const dummyMemos = [
   * 例
 
 ヘッケのL-函数はデデキントゼータ函数の自然な設定とリーマンゼータ函数の満たす函数等式に似た函数等式を持つ。`,
-  }
-]
+  },
+];
 
 const createDummy = Object.assign({}, dummyMemos[0]);
 const updateDummy = Object.assign({}, dummyMemos[1]);
